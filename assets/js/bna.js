4d1a985d0bdca3ac0c535ea577005ff4da0b2566
M H <haneyman@yahoo.com>
2012-10-16 16:50:04 -0700

    synch'd the starttimes radio with selected

--- assets/js/bna.js
+++ assets/js/bna.js
@@ -79,7 +79,7 @@ function loadTripTimes() {
     $("#radioTrips").append("<br/>Past -<br/>");
     for (i=0; i < arrayTripsPast.length; i++) {
         trip = arrayTripsPast[i];
-        str = "<input type='radio' name='radio-startTime' id='radio-startTime-" + i + "' value='choice-" + i + "' />";// + " checked='checked' />"
+        str = "<input type='radio' name='radio-startTime' id='radio-startTime-" + i + "' value='" + i + "' />";// + " checked='checked' />"
         str += "<label for='radio-startTime-" + i + "'> " + formatDateToTime(trip[0]) + "</label>";
         $("#radioTrips").append(str);
     }
@@ -87,7 +87,7 @@ function loadTripTimes() {
     var cnt = arrayTripsPast.length;
     for (j=0; j < arrayTripsFuture.length; j++) {
         trip = arrayTripsFuture[j];
-        str = "<input type='radio' name='radio-startTime' id='radio-startTime-" + (j+cnt) + "' value='choice-" + (j+cnt) + "' />";// + " checked='checked' />"
+        str = "<input type='radio' name='radio-startTime' id='radio-startTime-" + (j+cnt) + "' value='" + (j+cnt) + "' />";// + " checked='checked' />"
         str += "<label for='radio-startTime-" + (j+cnt) + "'> " + formatDateToTime(trip[0]) + "</label>";
         $("#radioTrips").append(str);
     }
